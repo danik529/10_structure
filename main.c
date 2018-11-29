0ef0648d63e32eb2a26efb352a2a487226a9536a
danik529 <lsy12011@naver.com>
2018-11-29 16:33:19 +0900

    keyword.c

--- main.c
+++ main.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int is_whitespace(char c)
+{
+	if (c == ' '||
+	    c == '\t' ||
+	    c == '\n' ||
+	    c == '\r' ||
+	    c == '('||
+	    c == ')')
+  	    return 1; //공백글자다.
+	
+	    return 0; //유의미한 글자다.  
+}
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int fgetc_word(FILE* fp, char* word)
+{
+	char c;
+	int cnt;
 
+while ((c=fgetc(fp)) != EOF){ //1문자 받음 
+	    if (is_whitespace(c) == 0){ //유의미한 문자이면 나감 
+		    break; 
+		} 
+	}
+	if (c == EOF){ //최초 읽은 문자가 마지막일 때 
+	    return 0; 
+	}
+	
+    // 본격적으로 단어를 한글자씩 읽음 
+	cnt = 0;
+	word[cnt++] = c; //아까 읽은 첫글자 입력
+	word[cnt] = '\0'; //혹시 뒤에서 못 읽을수 있으므로 (한글자짜리 단어)
+	
+	while ((word[cnt]=fgetc(fp)) != EOF){ // 1문자 받음 
+	    if (is_whitespace(word[cnt]) == 1) // 공백문자면 받은거 취소하고 나감  
+	    {
+	    	word[cnt] = '\0';
+	    	break;
+		}
+		cnt++;
+	}
+   
+    return cnt;
+}
 int main(int argc, char *argv[]) {
     FILE* fp;
 	char name[100];
+	char word[100];
 	
 	//printf, scanf
 	printf("input the file name :");
@@ -21,10 +62,16 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	
-	printf("%c\n", fgetc(fp));
+	//word reading
+	while (fgetc_word(fp, word) !=0)
+	{
+		//word processing
+		count_word(word);
+	}
+	print_word();
 	
 	//fclose
 	fclose(fp);
- 
+	
 	return 0;
 }
